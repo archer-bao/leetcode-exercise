c8649ecd418229793140bab5d3e8018e5397b419
archer-bao <baoq1991@126.com>
2020-07-17 13:26:27 +0800

    Create algo.go

---
+++ 3. Longest Substring Without Repeating Characters/algo.go
@@ -0,0 +1,31 @@
+package algo
+
+func lengthOfLongestSubstring(s string) int {
+	if len(s) < 1 {
+		return 0
+	}
+
+	var (
+		charMap               = make(map[int32]int)
+		maxLength             = 0
+		uniqueCharStringStart = 0
+	)
+
+	for index, each := range s {
+		// 判断字符是否重复
+		if at, ok := charMap[each]; ok {
+			// 重复则将不重复字符串首字符位置指示符移动到重复字符后
+			if at >= uniqueCharStringStart {
+				uniqueCharStringStart = at + 1
+			}
+		}
+
+		if index-uniqueCharStringStart+1 > maxLength {
+			maxLength = index - uniqueCharStringStart + 1
+		}
+
+		charMap[each] = index
+	}
+
+	return maxLength
+}
