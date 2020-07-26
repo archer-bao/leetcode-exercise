775aff0ab942d050945c55b2fa0ea2ffdd4c7ea3
archer-bao <baoq1991@126.com>
2020-07-26 22:41:46 +0800

    Update algo_test.go

--- 7. Reverse Integer/algo_test.go
+++ 7. Reverse Integer/algo_test.go
@@ -1,1 +1,35 @@
+package algo
 
+import (
+	"testing"
+)
+
+func TestReverse(t *testing.T) {
+	if reverse(12345) != 54321 {
+		t.Fatal()
+	}
+
+	if reverse(-12345) != -54321 {
+		t.Fatal()
+	}
+
+	if reverse(54321) != 12345 {
+		t.Fatal()
+	}
+
+	if reverse(789) != 987 {
+		t.Fatal()
+	}
+
+	if reverse(7890) != 987 {
+		t.Fatal()
+	}
+
+	if reverse(1234567899) != 0 {
+		t.Fatal()
+	}
+
+	if reverse(-2147483412) != -2143847412 {
+		t.Fatal()
+	}
+}
