b73574fce1196325318fa937120b4dd2e754f2f1
archer-bao <baoq1991@126.com>
2020-07-27 15:47:20 +0800

    Create algo.go

---
+++ 0014. Longest Common Prefix/algo.go
@@ -0,0 +1,18 @@
+package algo
+
+func longestCommonPrefix(strs []string) string {
+	if len(strs) < 1 {
+		return ""
+	}
+
+	for i := 0; i < len(strs[0]); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
+				println(strs[0][:i])
+				return strs[0][:i]
+			}
+		}
+	}
+
+	return strs[0]
+}
