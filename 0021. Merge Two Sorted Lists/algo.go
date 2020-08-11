2cd67594264e1e945021d58ff5ec739d05cf1bcc
archer-bao <baoq1991@126.com>
2020-08-11 22:24:23 +0800

    Create algo.go

---
+++ 0021. Merge Two Sorted Lists/algo.go
@@ -0,0 +1,29 @@
+package algo
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	var dummy = &ListNode{}
+	var cur = dummy
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			cur.Next = l1
+			l1 = l1.Next
+		} else {
+			cur.Next = l2
+			l2 = l2.Next
+		}
+		cur = cur.Next
+	}
+
+	if l1 != nil {
+		cur.Next = l1
+	} else {
+		cur.Next = l2
+	}
+
+	return dummy.Next
+}
