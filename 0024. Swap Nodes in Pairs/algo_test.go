f0f58f1f74ac33a9f46de0af9ac9bf0e3f3b402a
archer-bao <baoq1991@126.com>
2020-08-18 11:19:10 +0800

    Update and rename README.md to algo_test.go

--- 0024. Swap Nodes in Pairs/algo_test.go
+++ 0024. Swap Nodes in Pairs/algo_test.go
@@ -8,7 +8,7 @@ func TestFunc(t *testing.T) {
 	var head = &ListNode{Val: 1}
 	head.Next = &ListNode{Val: 2}
 	head.Next.Next = &ListNode{Val: 3}
-	// head.Next.Next.Next = &ListNode{Val: 4}
+	head.Next.Next.Next = &ListNode{Val: 4}
 	res := F(head)
 
 	if res == nil {
